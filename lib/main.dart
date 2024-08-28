8b07d8068055cbe11f45dcfcbf1b561ae20b3f79
hasnath.chowdhury <hasnath.chowdhury@reddotdigitalit.com>
2024-08-28 22:35:52 +0600

    HIVE CRUD App //wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:easy_task/models/notes_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
@@ -6,6 +7,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var directory = await getApplicationDocumentsDirectory();
   Hive.init(directory.path);
+
+  Hive.registerAdapter(NoteAdapter());
+  await Hive.openBox<Note>('notes');
 
   runApp(MaterialApp(
     title: 'Easy Task',
@@ -22,6 +26,10 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final titleController = TextEditingController();
+  final descriptionController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,33 +58,52 @@ class _HomeState extends State<Home> {
                   ],
                 );
               }),
-          // FutureBuilder(
-          //     future: Hive.openBox('jami2'),
-          //     builder: (context, snapshot) {
-          //       return Column(
-          //         children: [
-          //           ListTile(
-          //             title: Text(snapshot.data!.get('name2').toString()),
-          //           )
-          //         ],
-          //       );
-          //     }),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-          var box = await Hive.openBox('jami');
-          box.put('name', 'j c');
-          box.put('details', {'age': '00', 'gender': 'male'});
-          print(box.get('name'));
-          print(box.get('details'));
-          print(box.get('details')['gender']);
-
-          // var box2 = await Hive.openBox('jami2');
-          // box2.put('name2', 'name 2');
+            _showMyDialog();
         },
         child: Icon(Icons.add),
       ),
     );
   }
+
+  Future<void> _showMyDialog() async {
+    return showDialog(context: context, builder: (context) {
+      return AlertDialog(
+        title: Text('Add Note'),
+        content: SingleChildScrollView(
+          child: ListView(
+            children: [
+              TextFormField(
+                controller: titleController,
+                decoration: InputDecoration(
+                  hintText: 'Enter title',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextFormField(
+                controller: descriptionController,
+                decoration: InputDecoration(
+                  hintText: 'Enter description',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text('Cancel')),
+          TextButton(onPressed: () {
+            Navigator.pop(context);
+          }, child: Text('Add')),
+        ],
+      );
+    });
+  }
+
 }
---
+++ lib/models/notes_model.dart
@@ -0,0 +1,12 @@
+import 'package:hive/hive.dart';
+part 'notes_model.g.dart';
+
+@HiveType(typeId: 0)
+class Note {
+  @HiveField(0)
+  String title = "";
+  @HiveField(1)
+  String description = "";
+
+  Note({required this.title, required this.description});
+}
