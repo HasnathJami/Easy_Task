88b482b2b981b03b78fc9bb1cae6d9c248b68d33
hasnath.jami.chowdhury <hasnath.jami.chowdhury@gmail.com>
2024-08-29 02:40:36 +0600

    Full CRUD Implementation with UI

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:easy_task/models/notes_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'boxes/boxes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,7 +28,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final titleController = TextEditingController();
   final descriptionController = TextEditingController();
 
@@ -36,74 +37,170 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Easy Keep"),
       ),
-      body: Column(
-        children: [
-          FutureBuilder(
-              future: Hive.openBox('jami'),
-              builder: (context, snapshot) {
-                return Column(
-                  children: [
-                    ListTile(
-                      title: Text(snapshot.data!.get('name').toString()),
-                      subtitle: Text(snapshot.data!.get('details').toString()),
-                      trailing: IconButton(
-                        onPressed: () {
-                          // snapshot.data!.put('name','edited name');
-                          snapshot.data!.delete('name');
-                          setState(() {});
-                        },
-                        icon: Icon(Icons.delete),
+      body: ValueListenableBuilder<Box<Note>>(
+        valueListenable: Boxes.getData().listenable(),
+        builder: (context, box, _) {
+          var data = box.values.toList().cast<Note>();
+          return ListView.builder(
+            itemCount: box.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Text(data[index].title.toString()),
+                          Spacer(),
+                          InkWell(
+                              onTap: (){
+                                deleteNote(data[index]);
+                              },
+                              child: Icon(Icons.delete, color: Colors.red)
+                          ),
+                          SizedBox(width: 15,),
+                          InkWell(
+                              onTap: (){
+                                _editDialog(data[index], data[index].title.toString(), data[index].description.toString());
+                              },
+                              child: Icon(Icons.edit)
+                          ),
+
+                        ],
                       ),
-                    )
-                  ],
-                );
-              }),
-        ],
+
+                      Text(data[index].description.toString()),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
-            _showMyDialog();
+          _addDialog();
         },
         child: Icon(Icons.add),
       ),
     );
   }
 
-  Future<void> _showMyDialog() async {
-    return showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        title: Text('Add Note'),
-        content: SingleChildScrollView(
-          child: ListView(
-            children: [
-              TextFormField(
-                controller: titleController,
-                decoration: InputDecoration(
-                  hintText: 'Enter title',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 20,),
-              TextFormField(
-                controller: descriptionController,
-                decoration: InputDecoration(
-                  hintText: 'Enter description',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          TextButton(onPressed: () {
-            Navigator.pop(context);
-          }, child: Text('Cancel')),
-          TextButton(onPressed: () {
-            Navigator.pop(context);
-          }, child: Text('Add')),
-        ],
-      );
-    });
+  void deleteNote(Note note) async {
+    await note.delete();
   }
 
+  Future<void> _addDialog() async {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add Note'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    final data = Note(
+                        title: titleController.text,
+                        description: descriptionController.text);
+
+                    final box = Boxes.getData();
+                    box.add(data);
+                    // data.save();
+                    titleController.clear();
+                    descriptionController.clear();
+
+                    Navigator.pop(context);
+                  },
+                  child: Text('Add')),
+            ],
+          );
+        });
+  }
+
+  Future<void> _editDialog(Note note, String title, String description ) async {
+    titleController.text = title;
+    descriptionController.text = description;
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Edit Note'),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter title',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    controller: descriptionController,
+                    decoration: InputDecoration(
+                      hintText: 'Enter description',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Cancel')),
+              TextButton(
+                  onPressed: () async {
+                      note.title = titleController.text.toString();
+                      note.description = descriptionController.text.toString();
+                      note.save();
+
+                      titleController.clear();
+                      descriptionController.clear();
+
+                    Navigator.pop(context);
+                  },
+                  child: Text('Edit')),
+            ],
+          );
+        });
+  }
 }
--- lib/models/notes_model.dart
+++ lib/models/notes_model.dart
@@ -2,7 +2,7 @@ import 'package:hive/hive.dart';
 part 'notes_model.g.dart';
 
 @HiveType(typeId: 0)
-class Note {
+class Note extends HiveObject {
   @HiveField(0)
   String title = "";
   @HiveField(1)
